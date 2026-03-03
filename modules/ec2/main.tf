4a24a9fc5bbd2c2d1a7d6ded964127453339c785
Pankaj Masaye <48905347+pankajmasaye88@users.noreply.github.com>
2026-03-03 17:21:12 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,23 +1,23 @@
 resource "aws_instance" "public_vm" {
-  ami                    = "ami-02b8269d5e85954ef"
+  ami                    = "ami-019715e0d74f695be"
   instance_type          = var.instance_type
   subnet_id              = var.public_subnet_id
   key_name               = var.key_name
   vpc_security_group_ids = [var.sg_id]
 
   tags = {
-    Name = "alex-public-vm"
+    Name = "pankaj-public-vm"
   }
 }
 
 resource "aws_instance" "private_vm" {
-  ami                    = "ami-02b8269d5e85954ef"
+  ami                    = "ami-019715e0d74f695be"
   instance_type          = var.instance_type
   subnet_id              = var.private_subnet_id
   key_name               = var.key_name
   vpc_security_group_ids = [var.sg_id]
 
   tags = {
-    Name = "alex-private-vm"
+    Name = "pankaj-private-vm"
   }
 }
